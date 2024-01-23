87be31863dfa50d0a193cf7017d78e65ddf57659
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2024-01-24 00:00:15 +0300

    initial commit

---
+++ src/main/java/tech/kibetimmanuel/expensemanagerapi/ExpenseManagerApiApplication.java
@@ -0,0 +1,13 @@
+package tech.kibetimmanuel.expensemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerApiApplication.class, args);
+    }
+
+}
